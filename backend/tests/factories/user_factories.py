8f2bcbc2d9a9fe302edddbb628eba0383328a008
Jaap Roes <jroes@leukeleu.nl>
2024-06-19 16:06:48 +0200

    Test user model

--- backend/tests/factories/user_factories.py
+++ backend/tests/factories/user_factories.py
@@ -6,11 +6,15 @@ from factory.django import DjangoModelFactory
 class UserFactory(DjangoModelFactory):
     class Meta:
         model = "accounts.User"
+        skip_postgeneration_save = True
 
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
     email = factory.Faker("email")
     password = factory.django.Password("P@ssw0rd!")
+
+    # Call the `clean` method after creating the user
+    clean = factory.PostGenerationMethodCall("clean")
 
 
 class SuperUserFactory(UserFactory):
