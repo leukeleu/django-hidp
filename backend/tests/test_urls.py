7c0bbf5b4df222529e70496a6c3c21f1426c61ea
Jaap Roes <jroes@leukeleu.nl>
2024-06-24 17:12:50 +0200

    Current tests require a redirect on root

--- backend/tests/test_urls.py
+++ backend/tests/test_urls.py
@@ -1,7 +1,10 @@
+from django.contrib import admin
+from django.views.generic.base import RedirectView
 from django.urls import include, path
 
 from hidp.config import urls as hidp_urls
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="auth:login"), name="root"),
     path("", include(hidp_urls)),
 ]
