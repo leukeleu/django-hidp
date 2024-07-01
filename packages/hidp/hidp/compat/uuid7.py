80a217f92b1d8603a41256b341d4dbff75a121ce
Jaap Roes <jroes@leukeleu.nl>
2024-07-01 13:35:19 +0200

    Apply lintfixes/noqa

--- packages/hidp/hidp/compat/uuid7.py
+++ packages/hidp/hidp/compat/uuid7.py
@@ -45,15 +45,15 @@ else:
         def get_counter_and_tail():
             rand = int.from_bytes(os.urandom(10))
             # 42-bit counter with MSB set to 0
-            counter = (rand >> 32) & 0x1ffffffffff
+            counter = (rand >> 32) & 0x1FFFFFFFFFF
             # 32-bit random data
-            tail = rand & 0xffffffff
+            tail = rand & 0xFFFFFFFF
             return counter, tail
 
-        global _last_timestamp_v7
-        global _last_counter_v7
+        global _last_timestamp_v7, _last_counter_v7  # noqa: PLW0603
 
-        import time
+        import time  # noqa: PLC0415
+
         nanoseconds = time.time_ns()
         timestamp_ms, _ = divmod(nanoseconds, 1_000_000)
 
@@ -64,7 +64,7 @@ else:
                 timestamp_ms = _last_timestamp_v7 + 1
             # advance the counter
             counter = _last_counter_v7 + 1
-            if counter > 0x3ffffffffff:
+            if counter > 0x3FFFFFFFFFF:  # noqa: PLR2004
                 timestamp_ms += 1  # advance the timestamp
                 counter, tail = get_counter_and_tail()
             else:
@@ -73,10 +73,10 @@ else:
         _last_timestamp_v7 = timestamp_ms
         _last_counter_v7 = counter
 
-        int_uuid_7 = (timestamp_ms & 0xffffffffffff) << 80
-        int_uuid_7 |= ((counter >> 30) & 0xfff) << 64
-        int_uuid_7 |= (counter & 0x3fffffff) << 32
-        int_uuid_7 |= tail & 0xffffffff
+        int_uuid_7 = (timestamp_ms & 0xFFFFFFFFFFFF) << 80
+        int_uuid_7 |= ((counter >> 30) & 0xFFF) << 64
+        int_uuid_7 |= (counter & 0x3FFFFFFF) << 32
+        int_uuid_7 |= tail & 0xFFFFFFFF
 
         # Manually set the variant and version bits, to avoid a
         # `ValueError('illegal version number')` when calling the UUID constructor
