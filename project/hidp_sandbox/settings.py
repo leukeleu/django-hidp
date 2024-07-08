338684a752659f866e767c24e13bda4aa6728c63
Thomas Kalverda <tkalverda@leukeleu.nl>
2024-07-08 13:54:24 +0200

    Add django_ratelimit and redis cache backend

--- project/hidp_sandbox/settings.py
+++ project/hidp_sandbox/settings.py
@@ -301,3 +301,18 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
 }
+
+# Cache settings
+
+_REDIS_SOCKET = config.getliteral("app", "redis_socket", fallback=None)
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": (
+            f"unix://{_REDIS_SOCKET}?db=0" if _REDIS_SOCKET else "redis://redis:6379/1",
+        ),
+    },
+}
+
+del _REDIS_SOCKET  # Remove the _REDIS_SOCKET from the global namespace
