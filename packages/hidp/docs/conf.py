b175bf9aa0a4d9ec1257b1be5b6b14789cac8a5b
Thomas Kalverda <tkalverda@leukeleu.nl>
2024-07-10 15:27:25 +0200

    Add Sphinx, run sphinx-quickstart and add extensions

---
+++ packages/hidp/docs/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Headless Identity Provider"
+project_copyright = "2024, Jaap Roes, Dennis Bunskoek, Thomas Kalverda"
+author = "Jaap Roes, Dennis Bunskoek, Thomas Kalverda"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser"]
+myst_enable_extensions = ["colon_fence", "attrs_inline"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
