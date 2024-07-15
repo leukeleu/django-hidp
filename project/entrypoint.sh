618fd12d6c04ed99fda3ba7774957ef8a59b17ff
Jaap Roes <jroes@leukeleu.nl>
2024-07-15 15:52:14 +0200

    Install both project and package local requirements

--- project/entrypoint.sh
+++ project/entrypoint.sh
@@ -1,30 +1,48 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e # Exit on error
 
 # Make sure uv is up-to-date
 sudo python -m pip install --root-user-action=ignore -U uv
 
-_VENV="../var/venv"
-_FROZEN_REQUIREMENTS="../var/requirements_frozen.txt"
+_VENV=$(realpath "../var/venv")
+_FROZEN_PROJECT_REQUIREMENTS=$(realpath "../var/requirements_project_frozen.txt")
+_FROZEN_PACKAGE_REQUIREMENTS=$(realpath "../var/requirements_package_frozen.txt")
 
 if [ ! -f "${_VENV}/bin/activate" ]; then
   echo "Creating virtual environment..."
   uv venv "${_VENV}"
   # Remove the frozen requirements file hash, to force installation of dependencies
-  rm -f "${_FROZEN_REQUIREMENTS}.sha1"
+  rm -f "${_FROZEN_PROJECT_REQUIREMENTS}.sha1" "${_FROZEN_PACKAGE_REQUIREMENTS}.sha1"
 fi
 
 echo "Gathering dependencies..."
-uv pip compile ./requirements_local.txt -o "${_FROZEN_REQUIREMENTS}" --upgrade --no-annotate --no-header -q
+
+# Project requirements
+uv pip compile ./requirements_local.txt -o "${_FROZEN_PROJECT_REQUIREMENTS}" --upgrade --no-annotate --no-header -q
+
+# Package requirements
+pushd "../packages/hidp"
+uv pip compile ./requirements_local.txt -o "${_FROZEN_PACKAGE_REQUIREMENTS}" --upgrade --no-annotate --no-header -q
+popd
 
 # Install dependencies if:
 # - sha1sum -c <file>.sha1 has a non-zero exit code because either:
 #   - Checksum file (<file>.sha1) does not exist
 #   - <file> has changed
-if ! sha1sum -c "${_FROZEN_REQUIREMENTS}.sha1"; then
+if ! sha1sum -c "${_FROZEN_PROJECT_REQUIREMENTS}.sha1" || ! sha1sum -c "${_FROZEN_PACKAGE_REQUIREMENTS}.sha1"; then
   echo "Installing dependencies..."
-  uv pip install -r "${_FROZEN_REQUIREMENTS}"
-  sha1sum "${_FROZEN_REQUIREMENTS}" > "${_FROZEN_REQUIREMENTS}.sha1"
+
+  # Project requirements
+  uv pip install -r "${_FROZEN_PROJECT_REQUIREMENTS}"
+
+  # Package requirements
+  pushd "../packages/hidp"
+  uv pip install -r "${_FROZEN_PACKAGE_REQUIREMENTS}"
+  popd
+
+  # Update checksums
+  sha1sum "${_FROZEN_PROJECT_REQUIREMENTS}" > "${_FROZEN_PROJECT_REQUIREMENTS}.sha1"
+  sha1sum "${_FROZEN_PACKAGE_REQUIREMENTS}" > "${_FROZEN_PACKAGE_REQUIREMENTS}.sha1"
 fi
 
 if [ ! -f '../var/oidc.key' ]; then
