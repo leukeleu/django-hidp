6aeccd0f0729cde88425b1c3b6b7e2a026abd3b3
Thomas Kalverda <tkalverda@leukeleu.nl>
2024-07-16 14:48:10 +0200

    Rewrite docstrings to google style
    Also reorder methods alphabetically

--- packages/hidp/hidp/accounts/models.py
+++ packages/hidp/hidp/accounts/models.py
@@ -28,8 +28,15 @@ class UserManager(auth_models.UserManager):
         Prefer using this method over instantiating the user model directly,
         as it ensures that the email address is normalized and the password is hashed.
 
-        Automatically sets `is_staff` to `False` and `is_superuser` to `False`,
-        unless explicitly set otherwise in `extra_fields`.
+        Automatically sets ``is_staff`` to ``False`` and ``is_superuser`` to ``False``,
+        unless explicitly set otherwise in ``extra_fields``.
+
+        Args:
+            email (``str``): The email address of the user.
+            password (``str``, `optional`): The password of the user.
+
+        Returns:
+            ``User``: The newly created user.
         """
         return super().create_user(
             username=email,
@@ -42,8 +49,15 @@ class UserManager(auth_models.UserManager):
         """
         Create a new superuser with the given email and password.
 
-        Automatically sets `is_staff` and `is_superuser` to `True`,
-        unless explicitly set otherwise in `extra_fields`.
+        Automatically sets ``is_staff`` and ``is_superuser`` to ``True``,
+        unless explicitly set otherwise in ``extra_fields``.
+
+        Args:
+            email (``str``): The email address of the user.
+            password (``str``, `optional`): The password of the user.
+
+        Returns:
+            ``User``: The newly created superuser.
         """
         return super().create_superuser(
             username=email,
@@ -55,59 +69,86 @@ class UserManager(auth_models.UserManager):
 
 class BaseUser(auth_models.AbstractUser):
     """
-    Extends the default Django user model, inheriting the following fields:
+    Abstract base class that extends Django's default user model.
 
-    * password (CharField):
-      Hashed password
-    * first_name (CharField, blank: True):
-      Given name
-    * last_name (CharField, blank: True):
-      Family name
-    * is_active (BooleanField, default: True):
-      Whether the user is active (allowed to log in)
-    * is_staff (BooleanField, default: False):
-      Whether the user is a staff member (allowed to log into the admin site
-    * is_superuser (BooleanField, default: False):
-      Whether the user is a superuser (allowed to do anything)
-    * groups (ManyToManyField):
-      Groups the user belongs to
-    * user_permissions (ManyToManyField):
-      Permissions the user has
-    * date_joined (DateTimeField, default: now):
-      Date and time when the user was created
-    * last_login (DateTimeField, default: None):
-      Date and time when the user last logged in
-      Populated by Django when the `django.contrib.auth.user_logged_in` signal is sent.
+    Attributes:
+        password (``CharField``):
+            Hashed password.
 
-    Alters the default Django user model with the following modifications:
+        first_name (``CharField``):
+            Given name, `optional`
 
-    * Primary key (id) is a UUID field populated with a version 7 UUID
-    * The email field is case-insensitive, unique and required
-    * The username field is removed in favor of the email field
+        last_name (``CharField``):
+            Family name, `optional`
 
-    Adds the following fields:
+        is_active (``BooleanField``):
+            Whether the user is active (allowed to log in).
 
-    * email_verified (DateTimeField, default: None):
-      Date and time when the email address was verified
-    * last_modified (DateTimeField, auto_now: True):
-      Date and time when the user was last modified
+            Defaults to ``True``.
 
-    Django's user model defines some convenience properties and methods:
+        is_staff (``BooleanField``):
+            Whether the user is a staff member (allowed to log into the admin site).
 
-    * `is_anonymous` (property):
-      Always `False` (as opposed to always `True` for `AnonymousUser`)
-    * `is_authenticated` (property):
-      Always `True` (as opposed to always `False` for `AnonymousUser`)
-    * `clean` (method):
-      Normalizes the email address by lower-casing the domain part.
-      Always call this method, after setting the email address, before saving the user.
-    * `get_full_name` (method):
-      Returns first and last name joined by a space.
-    * `get_short_name` (method):
-      Returns the first name.
-    * `has_usable_password` (method):
-      Returns `True` if the user has a password set and it doesn't begin with the
-      unusable password prefix.
+            Defaults to ``False``.
+
+        is_superuser (``BooleanField``):
+            Whether the user is a superuser (allowed to do anything).
+
+            Defaults to ``False``.
+
+        groups (``ManyToManyField``):
+            Groups the user belongs to
+
+        user_permissions (``ManyToManyField``):
+            Permissions the user has
+
+        date_joined (``DateTimeField``):
+            Date and time when the user was created.
+
+            Defaults to the current date and time.
+
+        last_login (``DateTimeField``):
+            Date and time when the user last logged in
+            Populated by Django when the ``django.contrib.auth.user_logged_in`` signal
+            is sent.
+
+            Defaults to ``None``.
+
+    .. rubric:: Alters the default Django user model with the following modifications
+        :heading-level: 3
+
+    Attributes:
+        id (``UUIDField``):
+            Primary key, a version 7 UUID.
+
+            Altered:
+                Was ``int`` in the default Django user model.
+
+        email (``EmailField``):
+            Email address, case-insensitive, unique and required.
+
+            Altered:
+                Was case-sensitive, not unique and optional in the default Django
+                user model.
+
+        username (``None``):
+            Altered:
+                The username field is removed in favor of the email field
+
+    .. rubric:: Adds the following attributes
+        :heading-level: 3
+
+    Attributes:
+        email_verified (``DateTimeField``):
+            Date and time when the email address was verified.
+
+            Defaults to ``None``.
+
+        last_modified (``DateTimeField``):
+            Date and time when the user was last modified. Populated by Django when
+            the user is saved.
+
+            Defaults to the current date and time.
     """
 
     # Change the primary key to UUID
@@ -140,11 +181,57 @@ class BaseUser(auth_models.AbstractUser):
             )
         ]
 
+    def check_password(self, raw_password):
+        """
+        Check the raw password against the user's hashed password.
+
+        When the password is correct, but uses an outdated hashing algorithm,
+        the password is upgraded to use the latest algorithm.
+
+        Will save the user if the password is upgraded.
+
+        Args:
+            raw_password (``str``): The raw password to be checked.
+
+        Returns:
+            ``True`` if the password is correct, ``False`` otherwise.
+
+        """
+        return super().check_password(raw_password)
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """
+        Email this user with the given subject and message.
+
+        If ``from_email`` is not specified ``settings.DEFAULT_FROM_EMAIL`` is used.
+
+        Additional keyword arguments are passed to the `send_mail` function as-is.
+
+        Args:
+            subject (``str``): The subject of the email.
+            message (``str``): The message of the email.
+            from_email (``str``, `optional`): The sender's email address.
+        """
+        super().email_user(subject, message, from_email=from_email, **kwargs)
+
+    def save(self, *args, update_fields=None, **kwargs):
+        """
+        Save the user to the database.
+
+        Altered:
+            Always normalizes the email address before saving.
+        """
+        self.clean()  # Always normalize the email address
+        super().save(*args, update_fields=update_fields, **kwargs)
+
     def set_password(self, raw_password):
         """
         Set the user's password field to the hashed value of the raw password.
 
         The user is **not** saved after setting the password.
+
+        Args:
+            raw_password (``str``): The raw password to be hashed.
         """
         super().set_password(raw_password)
 
@@ -153,33 +240,6 @@ class BaseUser(auth_models.AbstractUser):
         Set the user's password field to a value that will never be a valid hash.
         """
         super().set_unusable_password()
-
-    def check_password(self, raw_password):
-        """
-        Check the raw password against the user's hashed password.
-
-        Returns `True` if the password is correct, `False` otherwise.
-
-        When the password is correct, but uses an outdated hashing algorithm,
-        the password is upgraded to use the latest algorithm.
-
-        Will save the user if the password is upgraded.
-        """
-        return super().check_password(raw_password)
-
-    def email_user(self, subject, message, from_email=None, **kwargs):
-        """
-        Email this user with the given subject and message.
-
-        If `from_email` is not specified `settings.DEFAULT_FROM_EMAIL` is used.
-
-        Additional keyword arguments are passed to the `send_mail` function as-is.
-        """
-        super().email_user(subject, message, from_email=from_email, **kwargs)
-
-    def save(self, *args, update_fields=None, **kwargs):
-        self.clean()  # Always normalize the email address
-        super().save(*args, update_fields=update_fields, **kwargs)
 
 
 class User(BaseUser):
