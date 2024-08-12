f6f1fde07feb7b50b59c55e9178ed15647ca1598
Thomas Kalverda <tkalverda@leukeleu.nl>
2024-08-12 15:27:19 +0200

    Add documentation related to rate limits and security considerations

--- packages/hidp/docs/conf.py
+++ packages/hidp/docs/conf.py
@@ -21,6 +21,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 autodoc_mock_imports = ["django"]
 
+myst_heading_anchors = 3
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
