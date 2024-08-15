0f45c1b947ad5a91f4c30c434524f85b8080cec1
Jaap Roes <jroes@leukeleu.nl>
2024-08-15 16:25:28 +0200

    Add OIDCRegistrationTokenGenerator

---
+++ packages/hidp/hidp/federated/tokens.py
@@ -0,0 +1,43 @@
+import uuid
+
+from datetime import timedelta
+
+from django.core import signing
+
+
+class BaseTokenGenerator:
+    key_salt = NotImplemented
+    token_timeout = NotImplemented
+
+    def _get_signer(self):
+        return signing.TimestampSigner(algorithm="sha256", salt=self.key_salt)
+
+    def make_token(self):
+        """
+        Generate an expiring token based on a random value.
+
+        Returns:
+            str: The generated token.
+        """
+        value = str(uuid.uuid4())
+        return self._get_signer().sign(value)
+
+    def check_token(self, token):
+        """
+        Verify the token.
+
+        Returns:
+            bool: True if the token is valid, False otherwise.
+        """
+        try:
+            self._get_signer().unsign(token, max_age=self.token_timeout)
+        except signing.BadSignature:
+            return False
+        return True
+
+
+class OIDCRegistrationTokenGenerator(BaseTokenGenerator):
+    """Token for the OIDC registration process."""
+
+    key_salt = "oidc-registration"
+    token_timeout = timedelta(minutes=15).total_seconds()
