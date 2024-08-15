abecb7288f9e2ed2fd8469b02c362a7235d29e54
Jaap Roes <jroes@leukeleu.nl>
2024-08-15 14:14:20 +0200

    Add OIDCRegistrationForm

---
+++ packages/hidp/hidp/federated/forms.py
@@ -0,0 +1,55 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.db import transaction
+
+from ..accounts.forms import TermsOfServiceMixin
+from .models import OpenIdConnection
+
+UserModel = get_user_model()
+
+
+class OIDCRegistrationForm(TermsOfServiceMixin, forms.ModelForm):
+    """
+    Create a user and OpenIDConnection from OIDC claims and user info.
+    """
+
+    agreed_to_tos = TermsOfServiceMixin.create_agreed_to_tos_field()
+
+    def __init__(self, *, provider_key, claims, user_info, **kwargs):
+        self.provider_key = provider_key
+        self.claims = claims
+        self.user_info = user_info
+        # Populate the form using the OIDC claims and user info.
+        oidc_data = claims | user_info
+        initial_data = {
+            "email": oidc_data.get("email"),
+            "first_name": oidc_data.get("given_name"),
+            "last_name": oidc_data.get("family_name"),
+        } | kwargs.pop("initial", {})
+        super().__init__(initial=initial_data, **kwargs)
+        # Disable the email field to prevent the user from changing it.
+        self.fields["email"].disabled = True
+
+    class Meta:
+        model = UserModel
+        fields = [
+            "email",
+            "first_name",
+            "last_name",
+        ]
+
+    @transaction.atomic
+    def save(self, *, commit=True):
+        user = super().save(commit=False)
+        user.set_unusable_password()
+        self.set_agreed_to_tos(user)
+        user.connection = OpenIdConnection(
+            user=user,
+            provider_key=self.provider_key,
+            issuer_claim=self.claims["iss"],
+            subject_claim=self.claims["sub"],
+        )
+        if commit:
+            user.save()
+            user.connection.save()
+        return user
---
+++ packages/hidp/tests/unit_tests/test_federated/test_forms.py
@@ -0,0 +1,69 @@
+from datetime import timedelta
+
+from django.test import TestCase
+from django.utils import timezone
+
+from hidp.federated import forms
+
+
+class TestOIDCRegistrationForm(TestCase):
+    def _create_form(self, data=None):
+        return forms.OIDCRegistrationForm(
+            data=data,
+            provider_key="test_provider",
+            claims={
+                "iss": "test_issuer",
+                "sub": "test_subject",
+                "email": "user@example.com",
+            },
+            user_info={
+                "given_name": "Firstname",
+                "family_name": "Lastname",
+            },
+        )
+
+    def test_data_from_claims_and_user_info(self):
+        """
+        The form is populated from the OIDC claims and user info.
+        """
+        form = self._create_form()
+        self.assertEqual(form.initial["email"], "user@example.com")
+        self.assertEqual(form.initial["first_name"], "Firstname")
+        self.assertEqual(form.initial["last_name"], "Lastname")
+
+    def test_tos_required(self):
+        """
+        Terms of service must be agreed to.
+        """
+        form = self._create_form(data={})
+        self.assertFalse(form.is_valid(), msg="Expected form to be invalid.")
+        self.assertFormError(form, "agreed_to_tos", "This field is required.")
+
+    def test_email_field_disabled(self):
+        """
+        Email field is disabled to prevent the user from changing it.
+        """
+        form = self._create_form(
+            data={
+                "email": "fake@example.com",
+                "agreed_to_tos": "on",
+            }
+        )
+        self.assertTrue(form.is_valid(), msg="Expected form to be valid.")
+        self.assertEqual(form.instance.email, "user@example.com")
+
+    def test_creates_user_and_connection(self):
+        """
+        The form creates a user and OpenIdConnection from OIDC claims and user info.
+        """
+        form = self._create_form(data={"agreed_to_tos": "on"})
+        self.assertTrue(form.is_valid(), msg="Expected form to be valid.")
+        user = form.save()
+        self.assertAlmostEqual(
+            user.agreed_to_tos, timezone.now(), delta=timedelta(seconds=10)
+        )
+        connection = user.openid_connections.first()
+        self.assertIsNotNone(connection, msg="Expected connection to be created.")
+        self.assertEqual(connection.provider_key, "test_provider")
+        self.assertEqual(connection.issuer_claim, "test_issuer")
+        self.assertEqual(connection.subject_claim, "test_subject")
