41cf3c88770eb416f851e2ec8e1f3a8190f91aef
Thomas Kalverda <tkalverda@leukeleu.nl>
2024-09-05 14:35:53 +0200

    Add LoginRequiredMixin

--- packages/hidp/tests/smoke_tests/test_accounts/test_management.py
+++ packages/hidp/tests/smoke_tests/test_accounts/test_management.py
@@ -12,6 +12,14 @@ class TestManageAccountView(TestCase):
         cls.user = user_factories.UserFactory()
         cls.manage_account_url = reverse("hidp_accounts:manage_account")
 
+    def test_login_required(self):
+        """Anonymous users should be redirected to the login page."""
+        response = self.client.get(self.manage_account_url)
+        self.assertRedirects(
+            response,
+            f"{reverse('hidp_accounts:login')}?next={self.manage_account_url}",
+        )
+
     def test_get(self):
         """The manage account page should be displayed for authenticated users."""
         self.client.force_login(self.user)
