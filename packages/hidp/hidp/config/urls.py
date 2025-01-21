a4e7be0fbc29efff42bd5347949724597c1384b9
Wouter de Vries <wdevries@leukeleu.nl>
2025-01-21 12:01:52 +0100

    Add otp url patterns if hidp.otp is in INSTALLED_APPS

--- packages/hidp/hidp/config/urls.py
+++ packages/hidp/hidp/config/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 from ..accounts import account_management_urls, account_urls
 from ..federated import oidc_client_urls, oidc_management_urls
+from ..otp import otp_management_urls
 
 urlpatterns = [
     path("", include(account_urls)),
@@ -10,6 +11,11 @@ urlpatterns = [
     path("manage/", include(account_management_urls)),
     path("manage/oidc/", include(oidc_management_urls)),
 ]
+
+if "hidp.otp" in settings.INSTALLED_APPS:
+    urlpatterns += [
+        path("manage/otp/", include(otp_management_urls)),
+    ]
 
 if "hidp.oidc_provider" in settings.INSTALLED_APPS:
     urlpatterns += [
---
+++ packages/hidp/hidp/otp/otp_management_urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+app_name = "hidp_otp_management"
+
+urlpatterns = [
+]
