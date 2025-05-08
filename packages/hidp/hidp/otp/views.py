1bf99f030b3df8123a9bf83f0b1b89953f21f74a
Robin van Leeuwen <rvanleeuwen@leukeleu.nl>
2025-05-08 15:59:02 +0200

    Add logout_url to context_data device-setup view

--- packages/hidp/hidp/otp/views.py
+++ packages/hidp/hidp/otp/views.py
@@ -24,6 +24,7 @@ from hidp.otp.devices import (
 )
 from hidp.otp.forms import OTPSetupForm, VerifyStaticTokenForm, VerifyTOTPForm
 from hidp.rate_limit.decorators import rate_limit_default
+from hidp.otp.utils import user_needs_to_verify_otp
 
 from .decorators import otp_exempt
 from .mailers import (
@@ -208,7 +209,9 @@ class OTPSetupDeviceView(RedirectURLMixin, FormView):
             "recovery_codes": "\n".join(
                 self.backup_device.token_set.values_list("token", flat=True)
             ),
-            "logout_url": reverse("hidp_accounts:logout"),
+            "back_url": reverse("hidp_otp_management:manage"),
+            "logout_url": reverse("hidp_accounts:logout") 
+                if user_needs_to_verify_otp(self.request.user) else None,
         }
 
     def form_valid(self, form):
