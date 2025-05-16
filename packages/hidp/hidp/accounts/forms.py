7850c36a01996f085fd84fa8524d2a94d9895526
Fabian van der Post <fvanderpost@leukeleu.nl>
2025-05-16 15:34:27 +0200

    Fix password field names and make required when setting password

--- packages/hidp/hidp/accounts/forms.py
+++ packages/hidp/hidp/accounts/forms.py
@@ -292,16 +292,16 @@ class SetPasswordForm(auth_forms.SetPasswordForm):
     template_name = "hidp/accounts/management/forms/set_password_form.html"
 
     # Fields
-    password1 = forms.CharField(
+    new_password1 = forms.CharField(
         label=_("Password"),
-        required=False,
+        required=True,
         strip=False,
         widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
         help_text=django_password_validation.password_validators_help_text_html(),
     )
-    password2 = forms.CharField(
+    new_password2 = forms.CharField(
         label=_("Password confirmation"),
-        required=False,
+        required=True,
         widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
         strip=False,
         help_text=_("Enter the same password as before, for verification."),
