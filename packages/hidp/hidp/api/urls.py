f130875988d523cc6e457dd03987fa2778440b96
Fabian van der Post <fvanderpost@leukeleu.nl>
2025-11-03 13:17:37 +0100

    Add email verified and email verification resend views and routes
    ^ Conflicts:
    ^	packages/hidp/hidp/api/views.py

--- packages/hidp/hidp/api/urls.py
+++ packages/hidp/hidp/api/urls.py
@@ -2,7 +2,12 @@ from rest_framework.routers import DefaultRouter
 
 from django.urls import include, path
 
-from .views import LoginView, UserViewSet
+from .views import (
+    EmailVerificationResendView,
+    EmailVerifiedView,
+    LoginView,
+    UserViewSet,
+)
 
 router = DefaultRouter()
 router.register("users", UserViewSet, basename="user")
@@ -12,4 +17,10 @@ app_name = "api"
 urlpatterns = [
     path("", include(router.urls)),
     path("login/", LoginView.as_view(), name="login"),
+    path("email-verified/", EmailVerifiedView.as_view(), name="email_verified"),
+    path(
+        "email-verified/resend/",
+        EmailVerificationResendView.as_view(),
+        name="email_verified_resend",
+    ),
 ]
--- packages/hidp/hidp/api/views.py
+++ packages/hidp/hidp/api/views.py
@@ -9,6 +9,7 @@ from drf_spectacular.utils import (
 from oauth2_provider.contrib.rest_framework import OAuth2Authentication
 from rest_framework import mixins, viewsets
 from rest_framework.authentication import SessionAuthentication
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -98,3 +99,28 @@ class LoginView(GenericAPIView):
             base_url=request.build_absolute_uri("/"),
         ).send()
         return Response(status=HTTPStatus.UNAUTHORIZED)
+
+
+class EmailVerifiedView(GenericAPIView):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):  # noqa: PLR6301
+        return Response(
+            {"email_verified": bool(request.user.email_verified)}, status=HTTPStatus.OK
+        )
+
+
+class EmailVerificationResendView(GenericAPIView):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+    verification_mailer = EmailVerificationMailer
+
+    def post(self, request, *args, **kwargs):
+        if request.user.email_verified:
+            raise ValidationError("Email is already verified.")
+        self.verification_mailer(
+            request.user,
+            base_url=request.build_absolute_uri("/"),
+        ).send()
+        return Response({}, status=HTTPStatus.OK)
