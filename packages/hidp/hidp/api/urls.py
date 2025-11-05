4c1e4576cb90b6a880ffdc77df167d5724259897
Fabian van der Post <fvanderpost@leukeleu.nl>
2025-11-05 18:12:52 +0100

    Add password reset and reset confirmation views

--- packages/hidp/hidp/api/urls.py
+++ packages/hidp/hidp/api/urls.py
@@ -2,7 +2,11 @@ from rest_framework.routers import DefaultRouter
 
 from django.urls import include, path
 
-from .views import UserViewSet
+from .views import (
+    PasswordResetConfirmationView,
+    PasswordResetRequestView,
+    UserViewSet,
+)
 
 router = DefaultRouter()
 router.register("users", UserViewSet, basename="user")
@@ -11,4 +15,14 @@ app_name = "api"
 
 urlpatterns = [
     path("", include(router.urls)),
+    path(
+        "password-reset/",
+        PasswordResetRequestView.as_view(),
+        name="password_reset_request",
+    ),
+    path(
+        "password-reset/confirm/",
+        PasswordResetConfirmationView.as_view(),
+        name="password_reset_confirm",
+    ),
 ]
--- packages/hidp/hidp/api/views.py
+++ packages/hidp/hidp/api/views.py
@@ -1,3 +1,7 @@
+import logging
+
+from http import HTTPStatus
+
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import (
     OpenApiParameter,
@@ -7,14 +11,27 @@ from drf_spectacular.utils import (
 from oauth2_provider.contrib.rest_framework import OAuth2Authentication
 from rest_framework import mixins, viewsets
 from rest_framework.authentication import SessionAuthentication
+from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, update_session_auth_hash
 from django.http import Http404
 
-from .serializers import UserSerializer
+from hidp.accounts.mailers import (
+    PasswordResetRequestMailer,
+    SetPasswordMailer,
+)
+
+from .serializers import (
+    PasswordResetConfirmationSerializer,
+    PasswordResetRequestSerializer,
+    UserSerializer,
+)
 
 UserModel = get_user_model()
+
+logger = logging.getLogger(__name__)
 
 
 @extend_schema_view(
@@ -54,3 +71,70 @@ class UserViewSet(
         if self.kwargs.get(self.lookup_url_kwarg or self.lookup_field) == "me":
             return self.request.user
         raise Http404
+
+
+@extend_schema_view(
+    post=extend_schema(
+        responses={
+            HTTPStatus.NO_CONTENT: None,
+        },
+    )
+)
+class PasswordResetRequestView(GenericAPIView):
+    authentication_classes = []
+    permission_classes = []
+    serializer_class = PasswordResetRequestSerializer
+    password_reset_request_mailer = PasswordResetRequestMailer
+    set_password_mailer = SetPasswordMailer
+
+    def post(self, request, *args, **kwargs):
+        # Get user from serializer if it exists for given email
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data["user"]
+
+        # Send an password reset email if the user exists
+        if user:
+            mailer_class = (
+                self.password_reset_request_mailer
+                if user.has_usable_password()
+                else self.set_password_mailer
+            )
+            try:
+                mailer_class(
+                    user=user,
+                    base_url=self.request.build_absolute_uri("/"),
+                ).send()
+            except Exception:
+                # Do not leak the existence of the user. Log the error and
+                # continue as if the email was sent successfully.
+                logger.exception("Failed to send password reset email.")
+
+        return Response(status=HTTPStatus.NO_CONTENT)
+
+
+@extend_schema_view(
+    post=extend_schema(
+        responses={
+            HTTPStatus.NO_CONTENT: None,
+        },
+    )
+)
+class PasswordResetConfirmationView(GenericAPIView):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = PasswordResetConfirmationSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        new_password = serializer.validated_data["new_password"]
+        request.user.set_password(new_password)
+        request.user.save()
+
+        # Make sure the current sessions remains valid after the password change
+        update_session_auth_hash(request, request.user)
+
+        return Response(status=HTTPStatus.NO_CONTENT)
