d9bd41bda62364a76198168f0ecd4cb450d1fb3f
Fabian van der Post <fvanderpost@leukeleu.nl>
2025-11-05 18:13:07 +0100

    Add password reset and reset confirmation serializers

--- packages/hidp/hidp/api/serializers.py
+++ packages/hidp/hidp/api/serializers.py
@@ -1,6 +1,15 @@
 from rest_framework import serializers
 
+import django.core.exceptions as django_exceptions
+
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import (
+    validate_password,
+)
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.decorators import method_decorator
+from django.utils.translation import gettext_lazy as _
+from django.views.decorators.debug import sensitive_variables
 
 UserModel = get_user_model()
 
@@ -17,3 +26,40 @@ class UserSerializer(serializers.ModelSerializer):
             "email",
         ]
         read_only_fields = ["email"]
+
+
+@method_decorator(sensitive_variables(), name="validate")
+class PasswordResetRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def validate(self, attrs):  # noqa: PLR6301
+        """
+        Validates that the email corresponds to an active user.
+
+        Returns the user in the validated data if found, else None.
+        """
+        attrs["user"] = UserModel.objects.filter(
+            email__iexact=attrs["email"], is_active=True
+        ).first()
+        return attrs
+
+
+@method_decorator(sensitive_variables("value"), name="validate_token")
+@method_decorator(sensitive_variables("value"), name="validate_new_password")
+class PasswordResetConfirmationSerializer(serializers.Serializer):
+    token = serializers.CharField()
+    new_password = serializers.CharField()
+
+    def validate_token(self, value):
+        if not default_token_generator.check_token(self.context["request"].user, value):
+            raise serializers.ValidationError(_("Invalid or expired token."))
+        return value
+
+    def validate_new_password(self, value):
+        try:
+            validate_password(password=value, user=self.context["request"].user)
+        except django_exceptions.ValidationError:
+            raise serializers.ValidationError(
+                _("Password does not meet requirements.")
+            ) from None
+        return value
