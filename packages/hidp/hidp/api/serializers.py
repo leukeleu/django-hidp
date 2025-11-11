998563ee96f01cbff5850d7908f92b7e9ace62a6
Fabian van der Post <fvanderpost@leukeleu.nl>
2025-11-11 16:19:01 +0100

    Add some context to the PasswordResetConfirmationSerializer

--- packages/hidp/hidp/api/serializers.py
+++ packages/hidp/hidp/api/serializers.py
@@ -68,24 +68,33 @@ class PasswordResetRequestSerializer(serializers.Serializer):
 
 @method_decorator(sensitive_variables(), name="_validate_new_password")
 @method_decorator(sensitive_variables(), name="validate")
+@method_decorator(sensitive_variables(), name="get_user")
 class PasswordResetConfirmationSerializer(serializers.Serializer):
     token = serializers.CharField()
     uidb64 = serializers.CharField()
     new_password = serializers.CharField()
 
     def _validate_new_password(self, user, value):  # noqa: PLR6301
+        """
+        Validate that the password meets all validator requirements.
+
+        Raises a ValidationError if the password is invalid.
+        """
         try:
             validate_password(password=value, user=user)
-        except django_exceptions.ValidationError:
-            raise serializers.ValidationError(
-                _("Password does not meet requirements.")
-            ) from None
+        except django_exceptions.ValidationError as exc:
+            raise serializers.ValidationError(exc.messages) from None
 
-    def validate(self, attrs):
-        message = _("Invalid token or user ID.")
+    def get_user(self, uidb64):  # noqa: PLR6301
+        """
+        Taken from Django's `PasswordResetConfirmView.get_user`.
+
+        This is used so we consistently handle the uidb64 in both the template views and
+        API views when decoding the user ID and retrieving the user from the database.
+        """
         try:
             # urlsafe_base64_decode() decodes to bytestring
-            uid = urlsafe_base64_decode(attrs["uidb64"]).decode()
+            uid = urlsafe_base64_decode(uidb64).decode()
             user = UserModel.objects.get(pk=uid)
         except (
             TypeError,
@@ -95,9 +104,18 @@ class PasswordResetConfirmationSerializer(serializers.Serializer):
             django_exceptions.ValidationError,
         ):
             user = None
+        return user
+
+    def validate(self, attrs):
+        """
+        Validates that the token and uidb64 correspond to a valid user.
+
+        If valid, runs password validation on the new password.
+        """
+        user = self.get_user(attrs["uidb64"])
 
         if not user or not default_token_generator.check_token(user, attrs["token"]):
-            raise serializers.ValidationError(message)
+            raise serializers.ValidationError(_("Invalid token or user ID."))
 
         self._validate_new_password(user, attrs["new_password"])
         return attrs
